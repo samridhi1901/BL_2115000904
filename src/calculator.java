0d39577e648565c977948c63c522996e9004e3f0
samridhi singh <gurjarsam6@gmail.com>
2025-01-17 00:37:44 +0530

    Add calculator file

---
+++ src/calculator.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.prin3tln("Enter first number:");
+        double num1 = sc.nextDouble();
+        System.out.println("Enter second number:");
+        double num2 = sc.nextDouble();
+        System.out.println("Enter operation ");
+        char operator = sc.next().charAt(0);
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Invalid operator");
+                return;
+        }
+
+        System.out.println("Result: " + result);
+    }
+}
+
